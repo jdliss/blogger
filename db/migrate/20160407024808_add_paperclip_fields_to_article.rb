f5726a9993553c9c54205d0733e75eea14ec82c3
Jonathan <jonacom@lissismore.com>
2016-04-06 21:02:54 -0600

    added pics

---
+++ db/migrate/20160407024808_add_paperclip_fields_to_article.rb
@@ -0,0 +1,7 @@
+class AddPaperclipFieldsToArticle < ActiveRecord::Migration
+  def change
+    add_column :articles, :image_file_name,    :string
+    add_column :articles, :image_content_type, :string
+    add_column :articles, :image_file_size,    :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407022005) do
+ActiveRecord::Schema.define(version: 20160407024808) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
   end
 
   create_table "comments", force: :cascade do |t|
